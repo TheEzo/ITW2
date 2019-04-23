7d1931bc78cf7ce7732354c16677f96026540018
nxf39066 <tomas.willaschek@nxp.com>
2019-04-23 18:57:41 +0200

    [adsf

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,9 @@ $(document).ready(function(){
 
 var Web = {
 	init : function(){
-		if (document.cookie)
+		if(document.cookie.indexOf('status') == -1)
+			document.cookie = 'status=0';
+		if (Web.cookie())
 			Web.readmore();
 		else
 			Web.readless();
@@ -20,17 +22,24 @@ var Web = {
 	},
 
 	readmore : function(){
-		document.cookie = 's';
+		document.cookie = 'status=1';
 		$('#tohide').removeClass('hidden');
 		$('#readmore').addClass('hidden');
 		$('#readless').removeClass('hidden');
 	},
 
 	readless : function(){
-		document.cookie = '';
+		document.cookie = 'status=0';
 		$('#tohide').addClass('hidden');
 		$('#readmore').removeClass('hidden');
 		$('#readless').addClass('hidden');
+	},
+
+	cookie : function(){
+		var pos = document.cookie.indexOf('status') + 7;
+		if(document.cookie[pos] == '1')
+			return true;
+		return false;
 	}
 }
 
